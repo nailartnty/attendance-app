ec2baf1b8d8079e5af5b20168dfcb2cd30473c26
nailartnty <nnailartnty.06@gmail.com>
2025-01-29 10:14:44 +0700

    make a header component

---
+++ lib/UI/attend/components/header.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+Container buildHeader(BuildContext context) {
+  return Container(
+    height: 50,
+    decoration: BoxDecoration(
+      borderRadius: BorderRadius.only()
+      
+    ),
+  );
+}
